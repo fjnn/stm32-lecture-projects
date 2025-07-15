ac92ad4c11efcfabf430d95e285b02d913eefd36
Gizem <gizem.ates@hvl.no>
2025-07-15 22:22:09 +0200

    It stopped working, set 480cycle, and worked again

--- adc_multichannel_dma/Src/main.c
+++ adc_multichannel_dma/Src/main.c
@@ -213,7 +213,7 @@ static void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_3;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_144CYCLES;
+  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
